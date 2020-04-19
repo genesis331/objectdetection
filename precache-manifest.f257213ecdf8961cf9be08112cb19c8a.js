ac5d2e28748389835c690269da67739cf744df7b
genesis331 <cheah3838@gmail.com>
2020-04-19 10:01:38 +0800

    Updates

---
+++ precache-manifest.f257213ecdf8961cf9be08112cb19c8a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a72a05641f937c63d83be123a114510",
+    "url": "/objectdetection/index.html"
+  },
+  {
+    "revision": "8e43830c93e43560653a",
+    "url": "/objectdetection/static/css/main.ac34f940.chunk.css"
+  },
+  {
+    "revision": "5536c1206d0ff8bdbc92",
+    "url": "/objectdetection/static/js/2.ec5a281a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/objectdetection/static/js/2.ec5a281a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e43830c93e43560653a",
+    "url": "/objectdetection/static/js/main.30383553.chunk.js"
+  },
+  {
+    "revision": "e6d763342acd5001204c",
+    "url": "/objectdetection/static/js/runtime-main.0b1d3be9.js"
+  },
+  {
+    "revision": "32ec54889ebad28a523973c6d910fc23",
+    "url": "/objectdetection/static/media/VolteBold.32ec5488.otf"
+  },
+  {
+    "revision": "92c90f334cb53d08a7ed7dbddc87da68",
+    "url": "/objectdetection/static/media/githubBtnIcon.92c90f33.svg"
+  },
+  {
+    "revision": "e0852cb27af54c561b609aca357baa44",
+    "url": "/objectdetection/static/media/zixuLogo.e0852cb2.svg"
+  }
+]);
